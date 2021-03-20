f7f5105c82836f54ee3418ef9dd37937b790411c
Veeresh603 <veereshkavalagi31@gmail.com>
2021-03-20 12:24:08 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2963bb479dfb278ea99caff53b34d608'),
+    },
+  },
+});
