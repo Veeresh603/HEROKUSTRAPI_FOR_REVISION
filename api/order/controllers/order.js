b485dbd50c29b16b94d1a38c6cbe01b2cf066279
Veeresh603 <analogica@analogica.in>
2021-06-22 16:36:42 +0530

    order

---
+++ api/order/controllers/order.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+      const {user} = ctx.state
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.order.search({...ctx.query, user: user.id});
+    } else {
+      entities = await strapi.services.order.find({...ctx.query, user: user.id});
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.order }));
+  },
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const {user} = ctx.state
+
+    const entity = await strapi.services.order.findOne({ id, user: user.id });
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  }
+};
