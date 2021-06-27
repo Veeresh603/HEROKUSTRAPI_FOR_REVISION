cd7fd7ec47de4170c297daa07b06db680039c712
Veeresh603 <analogica@analogica.in>
2021-06-27 20:59:37 +0530

    asdsad

--- config/middleware.js
+++ config/middleware.js
@@ -3,7 +3,7 @@
 module.exports = {
     settings: {
         cors: {
-            origin: ['http://localhost:8000', 'http://localhost:1337', 'https://certisured.com', 'https://dev-certisured.netlify.app'],
+            origin: ['http://localhost:8000', 'http://localhost:1337', 'https://certisured.com', 'https://dev-certisured.netlify.app', 'https://anastrapiatlas.herokuapp.com'],
             credentials: true
         }
     }
