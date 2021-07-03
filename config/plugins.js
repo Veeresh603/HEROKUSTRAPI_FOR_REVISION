65302b9923800e2da002173716c52bef6640d6d4
Veeresh603 <analogica@analogica.in>
2021-07-03 17:45:07 +0530

    asdsd

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,14 @@
-module.exports = ({env}) => ({
+module.exports = ({ env }) => ({
+  // ...
   email: {
-      provider: 'sendinblue',
-      providerOptions: {
-          sendinblue_api_key: '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',
-          sendinblue_default_replyto: 'hello@certisured.com',
-          sendinblue_default_from: 'hello@cerisured.com',
-          sendinblue_default_from_name: 'Certisured',
-      },
+    provider: 'sendgrid',
+    providerOptions: {
+      apiKey: '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',
     },
-  });
+    settings: {
+      defaultFrom: 'users@certisured.com',
+      defaultReplyTo: 'users@certisured.com',
+    },
+  },
+  // ...
+});
--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -138,7 +138,8 @@ module.exports = {
           httpOnly: true,
           // secure: process.env.NODE_ENV === "production" ? true : false,
           maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
-          sameSite : 'lax'
+          sameSite : 'lax',
+          domain: process.env.NODE_ENV === "development" ? "localhost" : "https://dev-certisured.netlify.app",
      
         });
         ctx.send({
