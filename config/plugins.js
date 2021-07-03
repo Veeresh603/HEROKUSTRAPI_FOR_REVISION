a8cfa8a649feb48478ff66ed61d00d7914a7e3e4
Veeresh603 <analogica@analogica.in>
2021-07-03 22:14:59 +0530

    sadsa

--- config/plugins.js
+++ config/plugins.js
@@ -1,9 +1,9 @@
-module.exports = ({ env }) => ({
+module.exports = () => ({
   // ...
   email: {
     provider: 'sendgrid',
     providerOptions: {
-      apiKey: env('SENDGRID_API_KEY'),
+      apiKey: process.env.SENDGRID_API_KEY,
     },
     settings: {
       defaultFrom: 'users@certisured.com',
