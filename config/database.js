b0e2c73f205171d68040a3c929eabbd70a023137
Veeresh603 <analogica@analogica.in>
2021-07-04 11:35:53 +0530

    sadsa

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        host: env('DATABASE_HOST', 'cluster0.5wyr1.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-atlas'),
+        username: env('DATABASE_USERNAME', 'veeru'),
+        password: env('DATABASE_PASSWORD', 'veeru7737'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
