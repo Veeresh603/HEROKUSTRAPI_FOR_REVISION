884347e915f049e519ad79a73526fe61046d3359
Veeresh603 <analogica@analogica.in>
2021-07-06 15:27:58 +0530

    adasd

--- extensions/users-permissions/controllers/Auth.js
+++ extensions/users-permissions/controllers/Auth.js
@@ -139,7 +139,7 @@ module.exports = {
           // secure: process.env.NODE_ENV === "production" ? true : false,
           maxAge: 1000 * 60 * 60 * 24 * 14, // 14 Day Age
           sameSite : 'lax',
-          domain: process.env.NODE_ENV === "development" ? "localhost" : "https://dev-certisured.netlify.app",
+          domain: "https://dev-certisured.netlify.app",
      
         });
         ctx.send({
