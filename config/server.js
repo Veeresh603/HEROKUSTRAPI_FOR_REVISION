52e721e7ea7fc27f53707b9a7c49de7098d4077b
Veeresh603 <analogica@analogica.in>
2021-07-13 23:40:38 +0530

    asasd

--- config/server.js
+++ config/server.js
@@ -2,7 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   proxy: true,
-  url: process.env.PRODUCTION_URL,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '2963bb479dfb278ea99caff53b34d608'),
