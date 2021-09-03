917ed813202cb929227ead7c051226fb4e710db3
root <root@ThinkPad-T430-6fa06a45.localdomain>
2021-09-03 11:44:28 +0530

    askjk

--- config/middleware.js
+++ config/middleware.js
@@ -3,7 +3,7 @@
 module.exports = {
     settings: {
         cors: {
-            origin: ['http://localhost:8000', 'http://localhost:1337', 'https://certisured.com', 'https://dev-certisured.netlify.app', 'https://anastrapiatlas.herokuapp.com'],
+            origin: ['http://localhost:8000', 'http://localhost:1337', `http://192.168.29.36:1337` ,'https://certisured.com', 'https://dev-certisured.netlify.app', 'https://anastrapiatlas.herokuapp.com'],
             credentials: true
         }
     }
