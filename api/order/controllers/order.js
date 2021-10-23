2acdab27e73d7fc57e75e3acba40c996a026a752
akshay kavalagi <veereshkavalagi31@gmail.com>
2021-10-23 18:54:03 +0530

    first commit

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const { sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   /**
@@ -10,21 +10,29 @@ module.exports = {
    */
 
   async find(ctx) {
-      const {user} = ctx.state
+    const { user } = ctx.state;
     let entities;
     if (ctx.query._q) {
-      entities = await strapi.services.order.search({...ctx.query, user: user.id});
+      entities = await strapi.services.order.search({
+        ...ctx.query,
+        user: user.id,
+      });
     } else {
-      entities = await strapi.services.order.find({...ctx.query, user: user.id});
+      entities = await strapi.services.order.find({
+        ...ctx.query,
+        user: user.id,
+      });
     }
 
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.order }));
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.order })
+    );
   },
   async findOne(ctx) {
     const { id } = ctx.params;
-    const {user} = ctx.state
+    const { user } = ctx.state;
 
     const entity = await strapi.services.order.findOne({ id, user: user.id });
     return sanitizeEntity(entity, { model: strapi.models.order });
-  }
+  },
 };
